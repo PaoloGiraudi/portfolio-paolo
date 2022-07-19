1db1717e7cbe5dbb79858e344a287a89e5d7d64c
Paolo <pgiraudi94@gmail.com>
2022-07-19 16:45:15 +0200

    add page heading component

--- src/routes/work.svelte
+++ src/routes/work.svelte
@@ -1,1 +1,19 @@
-<p>Work page here</p>
+<script>
+  import MetaTitle from '$lib/components/meta-title.svelte';
+  import PageHeading from '$lib/components/page-heading.svelte';
+</script>
+
+<MetaTitle title="My work" />
+<PageHeading header="Take a look to some of my work.">
+  <p>Some of the projects I worked on.</p>
+</PageHeading>
+<div>Work projects grid here</div>
+
+<style>
+  div {
+    height: var(--content-height);
+  }
+  p {
+    font-size: var(--font-md);
+  }
+</style>
