41ccb11df8d46d2de94d1f7a0f062090e12888f7
PaoloGiraudi <pgiraudi94@gmail.com>
2023-04-09 18:02:07 +0200

    fix clip path firefox

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -91,7 +91,7 @@
       background-clip: text;
       -webkit-background-clip: text;
       -webkit-text-fill-color: transparent;
-      clip-path: inset(100%);
+      clip-path: inset(50%);
       transition: clip-path 0.1s cubic-bezier(0.29, 0.73, 0.74, 1.02);
     }
     .name:hover::before {
