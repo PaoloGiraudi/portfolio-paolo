f6b303e3bd867b27e2c7f2c976c7800b3050bf46
PaoloGiraudi <pgiraudi94@gmail.com>
2023-11-05 19:58:31 +0100

    add email contact

---
+++ src/lib/icons/email.svelte
@@ -0,0 +1,8 @@
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24">
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M2 2h20v20H2zm6 6h8c.55 0 1 .45 1 1v6c0 .55-.45 1-1 1H8c-.55 0-1-.45-1-1l.005-6c0-.55.445-1 .995-1zm4 4.5L8 10v5h8v-5zm0-1L8 9h8z"
+    clip-rule="evenodd"
+  />
+</svg>
--- src/lib/icons/index.ts
+++ src/lib/icons/index.ts
@@ -2,3 +2,4 @@ export { default as Sun } from './sun.svelte';
 export { default as Moon } from './moon.svelte';
 export { default as Github } from './github.svelte';
 export { default as Linkedin } from './linkedin.svelte';
+export { default as Email } from './email.svelte';
