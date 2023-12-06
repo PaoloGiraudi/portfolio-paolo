f0e73385182de870356e92b435b1e55793ac34c1
PaoloGiraudi <lp.web@pm.me>
2023-12-06 16:44:49 +0100

    bump

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,16 +2,16 @@ import { db } from '$lib/server/database';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
-  // const projects = await db.query.projects.findMany({
-  //   columns: {
-  //     id: true,
-  //     name: true,
-  //     description: true,
-  //     url: true
-  //   },
-  //   orderBy: (projects, { desc }) => [desc(projects.id)]
-  // });
+  const projects = await db.query.projects.findMany({
+    columns: {
+      id: true,
+      name: true,
+      description: true,
+      url: true
+    },
+    orderBy: (projects, { desc }) => [desc(projects.id)]
+  });
   return {
-    projects: []
+    projects
   };
 };
