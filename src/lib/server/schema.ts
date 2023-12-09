34ec3591c153e3bca352ed35ea031fbbd1737159
PaoloGiraudi <lp.web@pm.me>
2023-12-09 23:40:22 +0100

    avoid db calls, make website faster

--- src/lib/server/schema.ts
+++
@@ -1,9 +0,0 @@
-import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
-
-export const projects = sqliteTable('projects', {
-  id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
-  name: text('name').notNull(),
-  description: text('description').notNull(),
-  url: text('url').notNull(),
-  code_url: text('github_url')
-});
