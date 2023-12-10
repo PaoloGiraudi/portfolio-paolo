ebea67cf6a3cf84481e4faab48c5905015081b95
PaoloGiraudi <lp.web@pm.me>
2023-12-10 10:40:10 +0100

    minify output

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,15 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
+  build: {
+    minify: true,
+    cssMinify: true
+  },
   plugins: [sveltekit()],
   ssr: {
     noExternal: ['style-object-to-css-string']
-  }
+  },
+  appType: 'custom'
 };
 
 export default config;
